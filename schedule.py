e29f053147bc264f11e5be868d0ac2faae758087
Isabel Zhu <45726570+isabelbbz@users.noreply.github.com>
2019-01-12 23:36:56 -0500

    Add files via upload

---
+++ schedule.py
@@ -0,0 +1,14 @@
+#Don't forget to account for the time to travel from one activity to another
+
+totalhours = input('How many hours would you like to go out for?')
+print (totalhours)
+
+#Each event takes 2.5 hours to complete
+
+#Each food activity takes 1 hour to complete
+
+
+
+
+
+
